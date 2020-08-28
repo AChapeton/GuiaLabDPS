1c537b066a2ff08a1a11ae551e502ed1b3032e26
Andres Chapeton <andres.chapeton0206@gmail.com>
2020-08-27 21:38:53 -0600

    Final Ejemplo 1 - Conversor

--- Guia3/Conversor/src/app/app.module.ts
+++ Guia3/Conversor/src/app/app.module.ts
@@ -1,16 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { ComboBoxComponent } from "./combo-box/combo-box.component";
+
+//Agregar módulo de formulario
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, ComboBoxComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
